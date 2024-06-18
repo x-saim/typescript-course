70a2eb18871a017cab79e1c9cf74534c6001c846
x-saim <saimxamir@gmail.com>
2024-06-18 17:30:56 -0400

    complete task 5 covering conditional type rendering

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,9 +1,11 @@
-import Component from './starter/04-events';
+//import Component from './final/05-challenge';
+import Component from './starter/05-challenge';
 
 function App() {
   return (
     <main>
-      <Component />
+      <Component type='basic' name='susan' />
+      <Component type='advanced' name='anna' email='anna@gmail.com' />
     </main>
   );
 }
