5f6435a7258222c9e03a260625002db07837f554
x-saim <saimxamir@gmail.com>
2024-06-18 16:35:25 -0400

    progress on task 4: event handling, inline vs reference typing requirements

--- 02-typescript-react-essentials/src/starter/04-events/index.tsx
+++ 02-typescript-react-essentials/src/starter/04-events/index.tsx
@@ -1,8 +1,38 @@
+import { useState } from 'react';
+
 function Component() {
+  const [text, setText] = useState(''); //infer type
+  const [email, setEmail] = useState(''); //infer type
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
+      <h2>Events Example</h2>
+      <form className='form'>
+        <input
+          type='text'
+          className='form-input mb-1'
+          placeholder='Enter Text'
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <input
+          type='email'
+          className='form-input mb-1'
+          placeholder='Enter Email'
+          value={email}
+          onChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
