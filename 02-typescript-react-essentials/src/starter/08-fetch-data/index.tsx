b70f6b5e81940234d3eb475b389e3869bd5570c9
x-saim <saimxamir@gmail.com>
2024-06-19 17:45:56 -0400

    complete task 8: fetching data with react-query

--- 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
+++ 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
@@ -1,55 +1,26 @@
-import { useState, useEffect } from 'react';
-const url = 'https://www.course-api.com/react-tours-project';
-import { type Tour, tourSchema } from './types';
+import { fetchTours } from './types';
+import { useQuery } from '@tanstack/react-query';
 
 function Component() {
-  const [tours, setTours] = useState<Tour[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<string | null>(null);
+  const {
+    isPending,
+    isError,
+    error,
+    data: tours, //alias
+  } = useQuery({
+    queryKey: ['tours'],
+    queryFn: fetchTours,
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch tours...');
-        }
-        const rawData: Tour[] = await response.json();
-        const result = tourSchema.array().safeParse(rawData);
-
-        if (!result.success) {
-          console.log(result.error.message);
-          throw new Error('Failed to parse tours');
-        }
-        setTours(result.data);
-      } catch (error) {
-        const message =
-          error instanceof Error ? error.message : 'there was an error...';
-        setIsError(message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <h3>Loading...</h3>;
-  }
-
-  if (isError) {
-    return <h3>Error: {isError}</h3>;
-  }
+  if (isPending) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error: {error.message}</h2>;
 
   return (
     <div>
       <h2 className='mb-1'>Tours</h2>
       {tours.map((tour) => {
         return (
-          <p key={tour.id} className='mb-1'>
+          <p className='mb-1' key={tour.id}>
             {tour.name}
           </p>
         );
@@ -57,4 +28,5 @@ function Component() {
     </div>
   );
 }
+
 export default Component;
